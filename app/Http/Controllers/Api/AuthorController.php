e841009bae2c6ff33b25c77586aee2b2aadd1877
siddiq <msiddiq1400@gmail.com>
2023-02-09 09:28:25 +0500

    added model controller and migrations

---
+++ app/Http/Controllers/Api/AuthorController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    public function register(Request $request)
+    {
+        
+    }
+
+    public function login(Request $request)
+    {
+        
+    }
+
+    public function profie()
+    {
+        
+    }
+
+    public function logout()
+    {
+        
+    }
+}
---
+++ app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function createBook(Request $request)
+    {
+        
+    }
+
+    public function listBooks()
+    {
+        
+    }
+
+    public function singleBook($id)
+    {
+        
+    }
+
+    public function updateBook(Request $request, $id)
+    {
+        
+    }
+
+    public function deleteBook($id)
+    {
+        
+    }
+}
