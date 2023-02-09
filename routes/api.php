cf04e504c9d36a3b50fadb4c58e7f9fc2345dd28
siddiq <msiddiq1400@gmail.com>
2023-02-09 09:33:56 +0500

    added apis

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AuthorController;
+use App\Http\Controllers\Api\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,21 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post("register", [AuthorController::class, 'register']);
+Route::post("login", [AuthorController::class, 'login']);
+
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::get('profile', [AuthorController::class, 'profile']);
+    Route::get('logout', [AuthorController::class, 'logout']);
+
+    //books
+    Route::post("create-book", [BookController::class, 'createBook']);
+    Route::get("list-books", [BookController::class, 'listBooks']);
+    Route::get("list-book/{id}", [BookController::class, 'singleBook']);
+    Route::post("update-book/{id}", [BookController::class, 'updateBook']);
+    Route::post("delete-book/{id}", [BookController::class, 'deleteBook']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
